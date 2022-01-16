216f331f2cfc0ea09a1a09aaae20eed84eaae8fb
xolugna <xolugna@gmail.com>
2022-01-16 11:32:10 +0100

    encodeuri nstead of manuale ncode

--- src/utils/tagTransform.ts
+++ src/utils/tagTransform.ts
@@ -1,4 +1,4 @@
 export const tagTransform = (tag: string) => {
-  tag = tag[0] === '#' ? tag.replace('#', '%23') : `%23${tag}`
-  return tag
+  tag = tag[0] === '#' ? tag : `#${tag}`
+  return encodeURIComponent(tag)
 }
